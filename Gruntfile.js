ccffe0461f6ed231ce7549f4ebbb14d5d324c0b6
Bastian Hofmann <bastian.hofmann@researchgate.net>
2015-08-25 18:10:06 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+/*
+ * grunt-html-sniffer
+ * https://github.com/researchgate/grunt-html-sniffer
+ *
+ * Copyright (c) 2015 ResearchGate GmbH
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.option('stack', true);
+
+    grunt.initConfig({
+        htmlsniffer : {
+            dev : {
+                options : {
+                    checkstyle : 'out/checkstyle.xml'
+                },
+                src : [
+                    '/home/researchgate/rg_dev/checkout/community/src/modules/*/stubs/**/*.html'
+                ],
+                checks : {
+                    classNameIsNotDynamic: {}
+                }
+            }
+        },
+        jshint : {
+            all : [
+                'Gruntfile.js',
+                'tasks/*.js',
+                '<%= nodeunit.tests %>'
+            ],
+            options : {
+                jshintrc : '.jshintrc'
+            }
+        }
+    });
+
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    //grunt.registerTask('test', ['htmlsniffer', 'nodeunit']);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'htmlsniffer:dev']);
+};
---
+++ tasks/htmlsniffer.js
@@ -0,0 +1,78 @@
+/*
+ * grunt-html-sniffer
+ * https://github.com/researchgate/grunt-html-sniffer
+ *
+ * Copyright (c) 2015 ResearchGate GmbH
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+var htmlparser = require('../lib/parsers/htmlparser'),
+    Promise = require('promise'),
+    Queue = require('promise-queue'),
+    glob = Promise.denodeify(require('glob')),
+    flatten = require('flatten'),
+    checks = require('../lib/checks/checks.js'),
+    reporters = require('../lib/reporters');
+
+Queue.configure(Promise);
+
+function getFilesFromPath(patterns, options) {
+    if (!patterns) {
+        grunt.fail.warn('No source file paths found.');
+    }
+
+    return Promise.all(patterns.map(function (pattern) {
+        return glob(pattern, options);
+    }));
+}
+
+function mapQueued(numConcurrent, keys, fn) {
+    var queue = new Queue(numConcurrent, Infinity);
+    var promises = keys.map(
+        function (key) {
+            return queue.add(
+                function () {
+                    return fn(key);
+                }
+            );
+        }
+    );
+    return promises;
+}
+
+module.exports = function (grunt) {
+    grunt.registerMultiTask('htmlsniffer', 'Sniffer for your HTML sources', function () {
+        var done = this.async(),
+            checksConfig = this.data.checks,
+            self = this;
+
+        var results = getFilesFromPath(this.data.src)
+            .then(flatten)
+            .then(function (files) {
+                return Promise.all(mapQueued(50, files, htmlparser));
+            })
+            .then(flatten);
+
+        results.then(
+            function (data) {
+                return Object.keys(checksConfig).map(
+                    function (checkName) {
+                        var check = checks[checkName],
+                            config = checksConfig[checkName];
+
+                        return check(data, config);
+                    }
+                );
+            }
+        )
+        .then(flatten)
+        .then(function (results) {
+            if (self.data.options.checkstyle) {
+                grunt.file.write(self.data.options.checkstyle, reporters.checkstyle(results));
+            }
+
+            done();
+        }).done();
+    });
+};
